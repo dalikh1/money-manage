a7f02eaf8840cd535a357f567efd01d5d0cc9cb0
dalikh1 <kheirdindali@gmail.com>
2025-03-01 13:20:53 +0100

    defining app route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
 
 const App = () => {
   return (
-    <div className='text-red-700'>
-      <h1>app</h1>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+const Root = () => {
+  // check if token existe in local storage
+  const isAuthenticated = !!localStorage.getItem("token"); 
+  // const isAuthenticated = localStorage.getItem("token") ? true : false;
+  // redirect to dashboard if Authenticated, otherwise to login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
+
+
